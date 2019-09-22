3d9083d15fca0eb9501d8e185b3a573a5b314e99
iqbal khan <iqbalhussainkhan@yahoo.com>
2019-09-22 16:30:49 +0400

    i swear final database :)

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const image = require('./controller/image');
 let db = knex({
   client: 'pg',
   connection:{
-  	host: process.env.DATABASE_URL,
+  	connectionString: process.env.DATABASE_URL,
   	ssl:true
   }
 });
